c9b64843b876bdf3af42774f582ca80e6e14080e
juhooon <126332022+juhooon@users.noreply.github.com>
2026-08-30 19:11:04 +0800

    Update build25.sh

--- rockchip/build25.sh
+++ rockchip/build25.sh
@@ -48,7 +48,7 @@ cat repositories
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES openssh-sftp-server"
-#PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-theme-argon"
